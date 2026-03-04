b8b4a387b9d009f236eca0fe065ac31fa1fd2870
maksymilan <3510040005@qq.com>
2026-03-04 10:07:46 +0800

    mha impl

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -30,7 +30,19 @@ class SimpleMultiHeadAttention:
         wv: mx.array,
         wo: mx.array,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        assert hidden_size % num_heads == 0
+        self.head_dim = hidden_size // num_heads
+        self.scale = mx.rsqrt(self.head_dim)
+        assert wq.shape == (hidden_size, num_heads * self.head_dim)
+        assert wk.shape == (hidden_size, num_heads * self.head_dim)
+        assert wv.shape == (hidden_size, num_heads * self.head_dim)
+        assert wo.shape == (num_heads * self.head_dim, hidden_size)
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
 
     def __call__(
         self,
@@ -39,7 +51,32 @@ class SimpleMultiHeadAttention:
         value: mx.array,
         mask: mx.array | None = None,
     ) -> mx.array:
-        pass
+        N, L ,_ = query.shape
+        assert query.shape == key.shape == value.shape
+        q_projection = (
+            linear(query, self.wq)
+            .reshape(N, L, self.num_heads, self.head_dim)
+            .transpose(0,2,1,3)
+        )
+        k_projection = (
+            linear(key, self.wk)
+            .reshape(N, L, self.num_heads, self.head_dim)
+            .transpose(0, 2, 1, 3)
+        )
+        v_projection = (
+            linear(value, self.wv)
+            .reshape(N, L, self.num_heads, self.head_dim)
+            .transpose(0, 2, 1, 3)
+        )
+        output = scaled_dot_product_attention_simple(
+            q_projection, 
+            k_projection, 
+            v_projection, 
+            scale=self.scale,
+            mask=mask
+        )
+        output = output.transpose(0, 2, 1, 3).reshape(N, L, self.hidden_size)
+        return linear(output, self.wo)
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
